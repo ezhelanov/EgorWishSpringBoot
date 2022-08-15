28bf2bebcfb8735ca5d41baf4ed03d1c2769536f
Egor Jelanov <egor.jelanov@masterdata.ru>
2022-08-15 14:52:51 +0300

    RTXgame

--- src/main/java/com/egor/top/repos/GameRepo.java
+++ src/main/java/com/egor/top/repos/GameRepo.java
@@ -1,8 +1,23 @@
 package com.egor.top.repos;
 
 import com.egor.top.models.GameModel;
+import com.egor.top.models.RTXGameModel;
 import com.egor.top.repos.norepositorybeans.AbstractEntertainmentRepo;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface GameRepo extends AbstractEntertainmentRepo<GameModel> {
 
+    @Query(value = "FROM RTXGameModel rtx")
+    List<RTXGameModel> findAllRTXGames();
+
+    @Query(value = "FROM GameModel g WHERE TYPE (g) = GameModel")
+    List<GameModel> findAllGames();
+
+    @Query(value = "FROM GameModel g WHERE TYPE (g) = :gameClass")
+    <T extends GameModel> List<T> findAllByClass(
+            @Param(value = "gameClass") Class<? extends GameModel> gameClass
+    );
 }
