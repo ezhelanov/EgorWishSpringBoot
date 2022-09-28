177e5c3c0f86082d825b84c02e1eee932840706e
Egor Jelanov <egor.jelanov@masterdata.ru>
2022-09-28 08:17:25 +0300

    Spring Security

--- src/main/java/com/egor/top/security/WebSecurityConfig.java
+++ src/main/java/com/egor/top/security/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package com.egor.top.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,7 +9,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
